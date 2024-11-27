e5aa44d272588ef2a1226ef711e4e933c8b8b174
qianchengawa <147665730+qianchengawa@users.noreply.github.com>
2024-11-27 20:38:25 +0800

    Update SDHub.lua

--- SDHub.lua
+++ SDHub.lua
@@ -35,7 +35,7 @@ function Load()
 end
 
 local Window = Rayfield:CreateWindow({
-	Name = "SDHub V2.1",
+	Name = "SDHub V2.21",
 	Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
 	LoadingTitle = "SDHub",
 	LoadingSubtitle = "by 牢大",
