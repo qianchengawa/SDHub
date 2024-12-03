0b9dc34102eb63f02200d207304ad6ec67abcf1e
qianchengawa <147665730+qianchengawa@users.noreply.github.com>
2024-12-03 11:58:28 +0800

    Update SDHub.lua

--- SDHub.lua
+++ SDHub.lua
@@ -35,7 +35,7 @@ function Load()
 end
 
 local Window = Rayfield:CreateWindow({
-	Name = "SDHub V2.25",
+	Name = "SDHub V2.21",
 	Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
 	LoadingTitle = "SDHub",
 	LoadingSubtitle = "by 牢大",
@@ -191,55 +191,74 @@ if game.PlaceId == 14279724900 then --游戏内
 	workspace.Scripted.TowerData.ChildAdded:Connect(function()
 		Dropdown:Refresh(GetTowersData())
 	end)
-	local function dec(bloon,vc,tm,tx,na,col)
+	local function dec(bloon,vc,tm,tx,na,col,lc)
 		if bloon == true then
 			for i,v in ipairs(tm:GetDescendants()) do
 				if v:IsA("Decal") and (v.Name == "Diamond" or v.Name == "Golden" or v.Name == "Cursed") then
 					v:Destroy()
+				elseif v:IsA("ParticleEmitter") or v:IsA("PointLight") or v:IsA("SurfaceLight") then
+					pcall(function()
+						v.Color = vc
+					end)
+					pcall(function()
+						v.Color = ColorSequence.new(vc)
+					end)
+					pcall(function()
+						v.Color = lc
+					end)
+					pcall(function()
+						v.Color = ColorSequence.new(lc)
+					end)
 				elseif v:IsA("BasePart") and v.Name ~= "HumanoidRootPart" and v.Name ~= "Query" then
 					v.Color = vc
-					local p = Instance.new("Decal")
-					p.Color3 = col
-					p.Texture = tx
-					p.Name = na
-					p.Face = Enum.NormalId.Back
-					p.ZIndex = 1
-					p.Parent = v
-					local p = Instance.new("Decal")
-					p.Color3 = col
-					p.Texture = tx
-					p.Name = na
-					p.Face = Enum.NormalId.Top
-					p.ZIndex = 1
-					p.Parent = v
-					local p = Instance.new("Decal")
-					p.Color3 = col
-					p.Texture = tx
-					p.Name = na
-					p.Face = Enum.NormalId.Left
-					p.ZIndex = 1
-					p.Parent = v
-					local p = Instance.new("Decal")
-					p.Color3 = col
-					p.Texture = tx
-					p.Name = na
-					p.Face = Enum.NormalId.Front
-					p.ZIndex = 1
-					p.Parent = v
-					local p = Instance.new("Decal")
-					p.Color3 = col
-					p.Texture = tx
-					p.Name = na
-					p.Face = Enum.NormalId.Right
-					p.ZIndex = 1
-					p.Parent = v
-					local p = Instance.new("Decal")
-					p.Color3 = col
-					p.Texture = tx
-					p.Name = na
-					p.Face = Enum.NormalId.Bottom
-					p.ZIndex = 1
-					p.Parent = v
+					if v.Material ~= Enum.Material.Neon then
+						local p = Instance.new("Decal")
+						p.Color3 = col
+						p.Texture = tx
+						p.Name = na
+						p.Face = Enum.NormalId.Back
+						p.ZIndex = 1
+						p.Parent = v
+						local p = Instance.new("Decal")
+						p.Color3 = col
+						p.Texture = tx
+						p.Name = na
+						p.Face = Enum.NormalId.Top
+						p.ZIndex = 1
+						p.Parent = v
+						local p = Instance.new("Decal")
+						p.Color3 = col
+						p.Texture = tx
+						p.Name = na
+						p.Face = Enum.NormalId.Left
+						p.ZIndex = 1
+						p.Parent = v
+						local p = Instance.new("Decal")
+						p.Color3 = col
+						p.Texture = tx
+						p.Name = na
+						p.Face = Enum.NormalId.Front
+						p.ZIndex = 1
+						p.Parent = v
+						local p = Instance.new("Decal")
+						p.Color3 = col
+						p.Texture = tx
+						p.Name = na
+						p.Face = Enum.NormalId.Right
+						p.ZIndex = 1
+						p.Parent = v
+						local p = Instance.new("Decal")
+						p.Color3 = col
+						p.Texture = tx
+						p.Name = na
+						p.Face = Enum.NormalId.Bottom
+						p.ZIndex = 1
+						p.Parent = v
+					else
+						pcall(function()
+							v.Color = lc
+						end)
+					end
 				end
 			end
 		else
@@ -259,13 +278,13 @@ if game.PlaceId == 14279724900 then --游戏内
 			local pz = unpack(Options)
 			if pz == "钻石" then
 				TowerData:SetAttribute("ShardType","Diamond")
-				dec(true,Color3.new(0, 123, 255),TowerModel,"http://www.roblox.com/asset/?id=18442360949","Diamond",Color3.new(1,1,1))
+				dec(true,Color3.new(0, 0.482353, 1),TowerModel,"http://www.roblox.com/asset/?id=18442360949","Diamond",Color3.new(1,1,1))
 			elseif pz == "诅咒" then
 				TowerData:SetAttribute("ShardType","Cursed")
-				dec(true,Color3.new(0,0,0),TowerModel,"http://www.roblox.com/asset/?id=1619172543","Cursed",Color3.new(0,0,0))
+				dec(true,Color3.new(0,0,0),TowerModel,"http://www.roblox.com/asset/?id=1619172543","Cursed",Color3.new(0,0,0),Color3.new(1, 0, 0))
 			elseif pz == "黄金" then
 				TowerData:SetAttribute("ShardType","Golden")
-				dec(true,Color3.new(255,170,0),TowerModel,"http://www.roblox.com/asset/?id=4685786041","Gold",Color3.new(1,1,1))
+				dec(true,Color3.new(1, 0.666667, 0),TowerModel,"http://www.roblox.com/asset/?id=4685786041","Gold",Color3.new(1,1,1))
 			elseif pz == "普通" then
 				TowerData:SetAttribute("ShardType",nil)
 				dec(false,nil,TowerModel)
